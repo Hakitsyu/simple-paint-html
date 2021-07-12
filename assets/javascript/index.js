ee6b4d42a14d6cbde6f671828a8bb7b3a4786d4f
vitor <vitorjdev@gmail.com>
2021-07-12 16:12:11 -0300

    More beautiful

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,11 +1,13 @@
 import PressingEvent from "./pressing-event.js";
 import { LineDrawer } from "./drawer.js";
 
+// HTML Elements
 const canvas = document.getElementById("root");
 const sizeInput = document.getElementById("size-input");
 const colorInput = document.getElementById("color-input");
 const generatedImg = document.getElementById("generated-img");
 const generateImgButton = document.getElementById("generate-img");
+
 const pressingEvent = new PressingEvent(canvas);
 const context = canvas.getContext("2d");
 const lineDrawer = new LineDrawer(context);
@@ -26,7 +28,6 @@ generateImgButton.addEventListener("click", () => generateImage());
 
 pressingEvent.on("startpress", (x, y) => lineDrawer.init(x, y));
 pressingEvent.on("pressing", (x, y) => lineDrawer.draw(x, y));
-
 
 function generateImage() {
     const dataURL = canvas.toDataURL();
